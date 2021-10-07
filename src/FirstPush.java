ed83c3dcbb7a9880615ec47b5986baa5144191ea
bamph1911 <pat14012@byui.edu>
2021-10-07 16:07:36 -0600

    Added "Here is a change"

--- src/FirstPush.java
+++ src/FirstPush.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class FirstPush {
+public class FirstPush {
+    public static void main(String[] args) {
+        System.out.println("First github push with Java!");
+        System.out.println("Here is a change!");
+    }
 }
